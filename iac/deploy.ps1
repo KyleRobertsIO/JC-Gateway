514ceacb60f3fe431a43824c02c97aa924bcfe72
Kyle Roberts <kyle.roberts@halton.ca>
2023-06-14 00:17:25 -0400

    Removed commnet

--- iac/deploy.ps1
+++ iac/deploy.ps1
@@ -6,17 +6,17 @@ az group create `
     --name $ResourceGroupName `
     --location $DeploymentLocation
 
-# az deployment group create `
-#     --resource-group $ResourceGroupName `
-#     --template-file main.bicep `
-#     --parameters location=$DeploymentLocation `
-#         deploymentEnv='dev' `
-#         vnetName='data-jobs' `
-#         containerGroupSubnetName='container-group-subnet' `
-#         containerGroupSubnetPrefix='10.0.1.0/24' `
-#         storageAccountName='acitesting' `
-#         storageAccountSubnetName='storage-account-subnet' `
-#         storageAccountSubnetPrefix='10.0.2.0/24'
+az deployment group create `
+    --resource-group $ResourceGroupName `
+    --template-file main.bicep `
+    --parameters location=$DeploymentLocation `
+        deploymentEnv='dev' `
+        vnetName='data-jobs' `
+        containerGroupSubnetName='container-group-subnet' `
+        containerGroupSubnetPrefix='10.0.1.0/24' `
+        storageAccountName='acitesting' `
+        storageAccountSubnetName='storage-account-subnet' `
+        storageAccountSubnetPrefix='10.0.2.0/24'
 
 $identity = az identity create `
     -g $ResourceGroupName `
