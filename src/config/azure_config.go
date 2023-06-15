a4b890e92a0af2ec404924eae9e84d8ff2b95d5f
Kyle Roberts <scuffedfox@Kyles-MacBook-Air.local>
2023-06-15 18:15:20 -0400

    Have AzureAuthType being validated on startup from .env

--- src/config/azure_config.go
+++ src/config/azure_config.go
@@ -1,11 +1,47 @@
 package config
 
-import "os"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type AzureAuthTypeEnum string
+
+const (
+	SERVICE_PRINCIPAL                AzureAuthTypeEnum = "SERVICE_PRINCIPAL"
+	USER_ASSIGNED_MANAGED_IDENTITY   AzureAuthTypeEnum = "USER_ASSIGNED_MANAGED_IDENTITY"
+	SYSTEM_ASSIGNED_MANAGED_IDENTITY AzureAuthTypeEnum = "SYSTEM_ASSIGNED_MANAGED_IDENTITY"
+)
+
+type AzureAuthType struct {
+	AzureAuthTypeEnum
+}
+
+func (aat *AzureAuthType) FromStr(typeStr string) error {
+	switch strings.ToUpper(typeStr) {
+	case "SERVICE_PRINCIPAL":
+		aat.AzureAuthTypeEnum = SERVICE_PRINCIPAL
+	case "USER_ASSIGNED_MANAGED_IDENTITY":
+		aat.AzureAuthTypeEnum = USER_ASSIGNED_MANAGED_IDENTITY
+	case "SYSTEM_ASSIGNED_MANAGED_IDENTITY":
+		aat.AzureAuthTypeEnum = SYSTEM_ASSIGNED_MANAGED_IDENTITY
+	default:
+		allowedOptions := "['SERVICE_PRINCIPAL', 'USER_ASSIGNED_MANAGED_IDENTITY', 'SYSTEM_ASSIGNED_MANAGED_IDENTITY']"
+		msg := "failed to determine provided AuthType from string;"
+		errMsg := fmt.Sprintf("%s acceptable values are %s", msg, allowedOptions)
+		return errors.New(errMsg)
+	}
+	return nil
+}
 
 type AzureAppConfig struct {
 	ClientID     string
 	ClientSecret string
 	Scope        string
+	AuthType     AzureAuthType
 }
 
 type AzureConfig struct {
@@ -14,10 +50,16 @@ type AzureConfig struct {
 }
 
 func assembleAzureAppConfig() AzureAppConfig {
+	authType := AzureAuthType{}
+	authTypeErr := authType.FromStr(os.Getenv("AZURE.APP.AUTH_TYPE"))
+	if authTypeErr != nil {
+		log.Fatal(authTypeErr.Error())
+	}
 	return AzureAppConfig{
 		ClientID:     os.Getenv("AZURE.APP.CLIENT_ID"),
 		ClientSecret: os.Getenv("AZURE.APP.CLIENT_SECRET"),
 		Scope:        os.Getenv("AZURE.APP.SCOPE"),
+		AuthType:     authType,
 	}
 }
 
